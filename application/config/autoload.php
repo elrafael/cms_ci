67b27d91bfec0319a0168384e7bca10c5008cbda
Rafael V. de Oliveira <elrafael123@gmail.com>
2014-01-07 11:41:07 +0000

    DataMapper configs
    In order to use DM we need to set the configs files and autoload some
    helpers and config’s file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database', 'session', 'template');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('inflector');
 
 
 /*
@@ -80,7 +80,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('datamapper');
 
 
 /*
